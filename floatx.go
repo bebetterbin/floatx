24c3e764c7592c2f3b704c80c95044362573500b
liaoxiangbin <liaoxiangbin.sichuan@foxmail.com>
2021-08-02 10:13:27 +0800

    add CeilFloat function to get the least value greater than or equal to the given value with fixed decimal digit

--- floatx.go
+++ floatx.go
@@ -2,7 +2,16 @@ package floatx
 
 import "math"
 
-// InDelta return ture if the abs difference value of expected and actual is in delta.
+// InDelta returns ture if the abs difference value of expected and actual is in delta.
 func InDelta(expected, actual, delta float64) bool {
 	return math.Abs(expected-actual) <= delta
+}
+
+// CeilFloat returns the least value greater than or equal to x, keep decimal decimal digit.
+func CeilFloat(x float64, decimal int) float64 {
+	d := float64(1)
+	if decimal > 0 {
+		d = math.Pow10(decimal)
+	}
+	return math.Ceil(x*d) / d
 }
--- floatx_test.go
+++ floatx_test.go
@@ -13,3 +13,15 @@ func TestInDelta(t *testing.T) {
 	assert.True(InDelta(1.23, 1.234, 1e-2))
 	assert.False(InDelta(1.23, 1.234, 1e-3))
 }
+
+func TestCeilFloat(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.InDelta(123.4, CeilFloat(123.4, 1), 1e-2)
+	assert.InDelta(123.5, CeilFloat(123.41, 1), 1e-2)
+	assert.InDelta(123.5, CeilFloat(123.49, 1), 1e-2)
+	assert.InDelta(124, CeilFloat(123.1, 0), 1e-1)
+	assert.InDelta(124, CeilFloat(123.9, 0), 1e-1)
+	assert.InDelta(124, CeilFloat(123.1, -1), 1e-1)
+	assert.InDelta(124, CeilFloat(123.9, -1), 1e-1)
+}
